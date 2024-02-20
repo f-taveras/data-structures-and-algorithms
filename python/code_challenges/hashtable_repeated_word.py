3643dd99eb5e99b2a59353a6f71042f0f985b682
Felix A. Taveras <felixataveras1@gmail.com>
2024-02-19 23:30:54 -0500

    switching machines

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,14 @@
-from data_structures.hashtable import Hashtable
+import re
+from collections import defaultdict
 
-
-def first_repeated_word():
-    pass
+def first_repeated_word(s):
+    # Remove punctuation and make lowercase
+    cleaned = re.sub(r'[^\w\s]', ' ', s.lower())
+    words = cleaned.split()
+    
+    word_counts = defaultdict(int)
+    for word in words:
+        word_counts[word] += 1
+        if word_counts[word] > 1:
+            return word
+    return None
